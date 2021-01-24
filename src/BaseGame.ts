8c2c60c2047987fcb2eb66ecb009b241499604d4
Patrik Radics <54003031+Codethier@users.noreply.github.com>
2021-01-24 06:32:01 +0100

    added skeleton methods

--- src/BaseGame.ts
+++ src/BaseGame.ts
@@ -18,11 +18,20 @@ export default abstract class BaseGame implements IBaseGame {
 
     /**
      * @returns {number}
-     * Egy random számot szorozz meg a this.level.length -el, 
+     * Egy random számot szorozz meg a this.level.length -el,
      * majd kerekítsd lefelé, ez lesz az index.
      * Majd térj vissza a this.levels tömbnek ezzel az indexével.
      */
-    abstract getRandomLevel(): Level;
+    getRandomLevel(length: number): Level {
+        let num: number = Math.random()
+        let intermed: number = num * length
+        let val: number = Math.floor(intermed)
+        return length[val]
+
+    }
+
+    ;
+
 
     /**
      * @returns {boolean}
@@ -30,17 +39,47 @@ export default abstract class BaseGame implements IBaseGame {
      * 2. hozz létre egy pick nevű változót, értéke random szám szorozva 100 -al
      * 3. térj vissza true értékkel, ha a pick kisebb int a chance
      */
-    abstract mayIHaveGoldenApple(): boolean;
+    mayIHaveGoldenApple(): boolean {
+        let chance: number = 56732
+        let pick = Math.random() * 100
+        return pick < chance
+    }
+
+    ;
 
     /**
      * @returns {void}
      * A metódus feladatai:
-     * 1. keresd meg a DOM -ban az összes .vertical-grid és .horizontal-grid 
+     * 1. keresd meg a DOM -ban az összes .vertical-grid és .horizontal-grid
      * elemet
      * 2. mentsd el őket egy grids nevű változóba
-     * 3. járd be a tömböt, és minden elemére hívd meg a Utils.removeNode 
+     * 3. járd be a tömböt, és minden elemére hívd meg a Utils.removeNode
      * metódust, hogy eltávolítsd őket az oldalról
      * 4. a this.gridVisible értékét állítsd false -ra
      */
-    abstract removeGrid (): void;
+    removeGrid(): void {
+        //queryselector is nodelistof but it does not ahve lsit methods?
+        let vertical: NodeListOf<any>= document.querySelectorAll('vertical-grid')
+        let horizontal: NodeListOf<any> = document.querySelectorAll('horizontal-grid')
+        let grids : object = vertical.concat(horizontal)
+
+
+
+
+
+    }
+
+
+
+    ;
+
+    currentLevel: Level | null;
+    food: Piece | null;
+    garden: HTMLDivElement;
+    goldenApple: Piece | null;
+    growth: number;
+    head: Piece;
+    length: number;
+    score: number;
+    tail: Piece;
 }
